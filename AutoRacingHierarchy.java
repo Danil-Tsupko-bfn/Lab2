1d2999e041d2e0b52865b1a33d4d6224d21c0fdb
Danil Tsupko <122647623+Danil-Tsupko-bfn@users.noreply.github.com>
2023-12-09 08:37:56 +0200

    Lab2

---
+++ AutoRacingHierarchy.java
@@ -0,0 +1,29 @@
+import org.example.JsonEntitySerialization;
+import org.example.XmlEntitySerialization;
+
+import java.io.IOException;
+
+public class AutoRacingHierarchy {
+    public static void main(String[] args) {
+        Formula1Car formula1Car = new Formula1Car("Mercedes", 2022, 1000, 500);
+        StreetCar streetCar = new StreetCar("Toyota", 2022, 200, true);
+
+        JsonEntitySerialization jsonSerialization = new JsonEntitySerialization();
+        try {
+            jsonSerialization.serialize(formula1Car, "formula1car.json");
+            Formula1Car deserializedFormula1Car = jsonSerialization.deserialize("formula1car.json", Formula1Car.class);
+            System.out.println("Deserialized Formula 1 Car: " + deserializedFormula1Car.getBrand());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        XmlEntitySerialization xmlSerialization = new XmlEntitySerialization();
+        try {
+            xmlSerialization.serialize(streetCar, "streetcar.xml");
+            StreetCar deserializedStreetCar = xmlSerialization.deserialize("streetcar.xml", StreetCar.class);
+            System.out.println("Deserialized Street Car: " + deserializedStreetCar.getBrand());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ org/example/JsonEntitySerialization.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.example.EntitySerialization;
+
+import java.io.File;
+import java.io.IOException;
+
+public class JsonEntitySerialization implements EntitySerialization {
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void serialize(Object entity, String fileName) throws IOException {
+        objectMapper.writeValue(new File(fileName), entity);
+    }
+
+    @Override
+    public <T> T deserialize(String fileName, Class<T> entityType) throws IOException {
+        return objectMapper.readValue(new File(fileName), entityType);
+    }
+}
